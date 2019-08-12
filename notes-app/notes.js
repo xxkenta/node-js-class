607b2fd73f0bb8fce872502820758081530f39d9
Kenta Ito <kai20834@mfc.corp.mckee.com>
2019-08-12 16:18:52 -0400

    finished notes app

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,4 @@
- const fs = require('fs')
+const fs = require('fs')
 const chalk = require('chalk')
 
 const getNotes = () => {
@@ -7,9 +7,9 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note) => note.title === title)
-
-    if (duplicateNotes.length === 0) {
+    const duplicateNote = notes.find((note) => note.title === title)
+  
+    if (!duplicateNote) {
         notes.push({
             title: title,
             body: body
@@ -48,12 +48,24 @@ const loadNotes = () => {
 const listNotes = () => {
     notes = loadNotes()
     console.log(chalk.blue.inverse('Your Notes!'))
-    notes.forEach((note) => console.log(note.title + '\n' + note.body + '\n'))
+    notes.forEach((note) => console.log(note.title))
+}
+
+const readNote = (title) => {
+    notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+    if(note){
+        console.log(chalk.greenBright(note.title))
+        console.log(note.body)
+    } else{
+        console.log(chalk.redBright.inverse("This note does not exist"))
+    }
 }
 
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNote: readNote
 }
